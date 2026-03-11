d9b2ce7ef8a529b74924f75fe36bd7f9ac48486e
HOkhavhe <h_okhavhe@mail.harpercollege.edu>
2026-03-11 16:27:39 -0500

    Add files via upload

---
+++ WEEK3/WEEK 3 CIS106.py
@@ -0,0 +1,59 @@
+02/02/26   Honour Okhavhe CIS106-001
+
+
+companyname=input("Input Company Name")
+number=float(input("ENTER NUMBER"))
+cost=float(input("SHARE COST"))
+
+numberinvested=number*cost
+
+
+print("amountinvested:   ",  numberinvested)
+
+lastname = input("enter student last name")
+midterm = float(input("enter midterm score"))
+Finalexam = float(input("enter Final exam score"))
+
+total = midterm + Finalexam 
+
+
+print("student:   ", lastname)
+print("total points earned  ", total)
+
+totalpay =float(input("enter total pay"))
+amount =totalpay/3
+
+print("mypay: ",amount)
+print("friend1 pay: ",amount)
+print("friend2 pay: ",amount)
+
+
+
+make = input("enter car make")
+model = input("enter car model")
+Msrp = float(input("enter car mrsp"))
+Discount = float(input("enter discount %"))
+
+
+Amountoff = (Msrp*Discount)/100
+Discountprice = Msrp-Amountoff
+
+
+print(make)
+print(Msrp)
+print(model)
+print(Discount)
+print(Amountoff)
+print(Discountprice)
+
+
+radius = float(input("enter circle radius"))
+pie = 3.174
+area = radius*radius*pie
+perimeter = 2*pie*radius
+
+
+print(area)
+print(perimeter)
+
+
